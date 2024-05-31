9fddd7b6eefefec9a26b586184bd92453249d66d
federicocaballero <fedecaballero1234@gmail.com>
2024-05-31 11:10:16 -0300

    Se agregaron los componentes y estilos para mostrar la informacion en el detalle de peliculas

---
+++ src/presentation/componnets/movie/MovieDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const MovieDetails = () => {
+  return (
+    <View style={{ marginHorizontal: 20 }}>
+      <View style={{ flexDirection: "row" }}>
+        <Text>movie details</Text>
+      </View>
+    </View>
+  );
+};
--- src/presentation/screens/details/DetailsScreen.tsx
+++ src/presentation/screens/details/DetailsScreen.tsx
@@ -3,17 +3,29 @@ import React from "react";
 import { View, Text } from "react-native";
 import { RootStackParams } from "../../navigation/Navigation";
 import { useMovie } from "../../hooks/useMovie";
+import { MovieHeader } from "../../componnets/movie/MovieHeader";
 
 interface Props extends NativeStackScreenProps<RootStackParams, "Details"> {}
 
 export const DetailsScreen = ({ route }: Props) => {
   const { movieId } = route.params;
 
-  const {} = useMovie(movieId);
+  const { isLoading, movie } = useMovie(movieId);
+  if (isLoading) {
+    return <Text>Loaging...</Text>;
+  }
   console.log(movieId);
+
   return (
     <View>
-      <Text>Details Screen</Text>
+      {/* HEADER */}
+      <MovieHeader
+        originalTitle={movie!.originalTitle}
+        poster={movie!.poster}
+        title={movie!.title}
+      />
+
+      {/* DETALLES */}
     </View>
   );
 };
